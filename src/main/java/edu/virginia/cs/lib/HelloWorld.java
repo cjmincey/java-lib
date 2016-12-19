25a3219deaba5cf78179bb0da0232fb474db97a4
Jordan <cjmincey@gmail.com>
2016-12-19 15:06:09 -0500

    trigger CI builds

--- src/main/java/edu/virginia/cs/lib/HelloWorld.java
+++ src/main/java/edu/virginia/cs/lib/HelloWorld.java
@@ -6,6 +6,6 @@ package edu.virginia.cs.lib;
  */
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello world\n");
+        System.out.println("Hello world!\n");
     }
 }
