345dac886998688a6365b85a781a9191983360f3
Jordan <cjmincey@gmail.com>
2016-12-19 14:47:36 -0500

    create basic maven repo

---
+++ src/main/java/edu/virginia/cs/lib/HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.virginia.cs.lib;
+
+/**
+ *
+ * @author Jordan
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world\n");
+    }
+}
